462769512053cff48a4f92f8e2fd76e93f2d144f
qlljcf666 <60305000+qlljcf666@users.noreply.github.com>
2026-06-12 17:36:36 +0800

    Update init.sh

--- target/linux/rockchip/armv8/base-files/lib/board/init.sh
+++ target/linux/rockchip/armv8/base-files/lib/board/init.sh
@@ -131,7 +131,8 @@ board_fixup_iface_name() {
 			rename_iface lan3 eth3
 		fi
 		;;
-	firefly,rk3568-roc-pc)
+	firefly,rk3568-roc-pc|\
+	firefly,onebox-g04)
 		device="$(get_iface_device eth0)"
 		if [[ "$device" = "fe010000.ethernet" ]]; then
 			rename_iface eth0 wan
@@ -217,7 +218,8 @@ board_set_iface_smp_affinity() {
 	case $(board_name) in
 	inspur,ihec301|\
 	ezpro,mrkaio-m68s|\
-	firefly,rk3568-roc-pc)
+	firefly,rk3568-roc-pc|\
+	firefly,onebox-g04)
 		set_iface_cpumask 2 eth0
 		set_iface_cpumask 4 eth1
 		;;
